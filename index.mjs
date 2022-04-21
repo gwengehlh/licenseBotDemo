7079da4d56699bc79c82c683c50c8396b4acd87f
Gwendolyn <gwengehlh@gmail.com>
2022-04-21 16:24:02 -0400

    allow bot to print incoming messages to console.

--- index.mjs
+++ index.mjs
@@ -8,4 +8,8 @@ client.once("ready", () => {
 	console.log("Bot is ready!");
 })
 
+client.on("messageCreate", (message) => {
+	console.log(message.content);
+})
+
 client.login("OTYzODczMzYwNjg3ODIwODIw.YlcbBQ.JcxPTJdtmBSr6wUMYsJV55PQG-s");
